853175c7de9ba9c19808ac385d4f317b1c478caa
Kaique Santos <kaiqueba6@gmail.com>
2022-03-24 18:28:50 -0300

    Meu primeiro commit

---
+++ js.js
@@ -0,0 +1,23 @@
+ 
+          let nome = ''
+              do {
+                   nome = prompt("Qual o seu nome?")
+              }while(nome.length === 0);
+              
+                    const idade = prompt('Qual é a sua idade?')
+                    const linguagem = prompt('Qual linguagem de programação você está estudando?')
+          
+          alert(`Você se chama: ${nome}, você tem ${idade}anos de idade, sua linguagem favorita é: ${linguagem}`);
+    
+          
+     
+         
+  
+          
+     
+         
+
+
+
+
+
